9ec32f77f98d089accf3a4b2161f20d1428b5cbb
Paulo Gandra de Sousa <pag@isep.ipp.pt>
2018-03-16 22:24:58 +0000

    UC report "high calories dishes"

--- ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/dishes/reporting/ReportDishesPerDishTypeUI.java
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/console/presentation/dishes/reporting/ReportDishesPerDishTypeUI.java
@@ -1,19 +1,20 @@
 package eapli.ecafeteria.app.backoffice.console.presentation.dishes.reporting;
 
-import eapli.ecafeteria.application.dishes.reporting.ReportDishesPerDishTypeController;
+import eapli.ecafeteria.application.dishes.reporting.DishReportingController;
 import eapli.ecafeteria.domain.dishes.reporting.DishesPerDishType;
 import eapli.framework.application.Controller;
 import eapli.framework.presentation.console.AbstractListUI;
 import eapli.framework.visitor.Visitor;
 
 /**
- * This classes lists dish types by using the AbstractListUI from the framework.
+ * This classes creates a report on the number of dishes per dish types by using
+ * the AbstractListUI from the framework.
  *
- * Created by MCN on 29/03/2016.
+ * @author PAG
  */
 public class ReportDishesPerDishTypeUI extends AbstractListUI<DishesPerDishType> {
 
-    private final ReportDishesPerDishTypeController theController = new ReportDishesPerDishTypeController();
+    private final DishReportingController theController = new DishReportingController();
 
     protected Controller controller() {
         return this.theController;
@@ -21,7 +22,7 @@ public class ReportDishesPerDishTypeUI extends AbstractListUI<DishesPerDishType>
 
     @Override
     protected Iterable<DishesPerDishType> elements() {
-        return this.theController.all();
+        return this.theController.reportDishesPerDishType();
     }
 
     @Override
@@ -36,6 +37,6 @@ public class ReportDishesPerDishTypeUI extends AbstractListUI<DishesPerDishType>
 
     @Override
     protected String listHeader() {
-        return "DISHES";
+        return "DISHES PER DISH TYPE";
     }
 }
