5c726491146d36c2b25460405b6db487880ff690
jsant <jsant@LAPTOP-O5VNMES8>
2018-05-01 19:09:06 +0100

    UC_06 Publish Menu update

---
+++ ecafeteria.app.backoffice.console/src/main/java/eapli/ecafeteria/app/backoffice/presentation/menu/PublishMenuAction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eapli.ecafeteria.app.backoffice.presentation.menu;
+
+import eapli.framework.actions.Action;
+
+/**
+ *
+ * @author jsant
+ */
+public class PublishMenuAction implements Action {
+    
+    @Override
+    public boolean execute(){
+        return new PublishMenuUI().show();
+    }
+    
+}
