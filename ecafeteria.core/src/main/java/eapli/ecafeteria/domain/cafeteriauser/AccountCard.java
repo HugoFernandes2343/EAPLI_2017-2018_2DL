aab4d66a7e47a40f95a3b8be998818299112fd33
jsant <jsant@LAPTOP-O5VNMES8>
2018-05-01 21:20:26 +0100

    problem solved

--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/AccountCard.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/AccountCard.java
@@ -4,7 +4,6 @@ import eapli.framework.domain.ddd.ValueObject;
 import java.io.Serializable;
 import javax.persistence.Embeddable;
 import javax.persistence.Transient;
-import javax.persistence.Version;
 
 /**
  *
@@ -12,7 +11,7 @@ import javax.persistence.Version;
  */
 @Embeddable
 public class AccountCard implements ValueObject, Serializable{
-    
+
     private static final long serialVersionUID = 1L;
     
     //every user starts with 0€ on balance 
@@ -21,14 +20,17 @@ public class AccountCard implements ValueObject, Serializable{
     //initial balance limit for warnings, must be configured
     private static final double INITIAL_BALANCE_LIMIT = -1;
 
-    @Version
-    private Long version;
+
     
     @Transient
     private double balance;
     
     
     private double balance_limit;
+    
+//    protected AccountCard(){
+//        //for ORM
+//    }
     
     /**
      * New Account Card Constructor and for ORM use
--- ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/CafeteriaUser.java
+++ ecafeteria.core/src/main/java/eapli/ecafeteria/domain/cafeteriauser/CafeteriaUser.java
@@ -33,7 +33,7 @@ public class CafeteriaUser implements AggregateRoot<MecanographicNumber>, Serial
     @EmbeddedId
     private MecanographicNumber mecanographicNumber;
     
-    @EmbeddedId
+//    @EmbeddedId
     private AccountCard accountcard;
 
     /**
